c156b29b5d367c67ddfdf3faacb09265a2853280
Ulil Amry <ulilamry432@gmail.com>
2025-02-12 17:04:20 +0700

    feat:add helper to handle bucket

---
+++ helpers/handle_bucket.go
@@ -0,0 +1,80 @@
+package helpers
+
+import (
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+func UploadFile(uploader *s3manager.Uploader, filePath string, bucketName string, fileName string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(fileName),
+		Body:   file,
+	})
+
+	return err
+}
+
+func ListObjects(client *s3.S3, bucketName string) (*s3.ListObjectsV2Output, error) {
+	res, err := client.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(bucketName),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func DownloadFile(downloader *s3manager.Downloader, bucketName string, key string, downloadPath string) error {
+	file, err := os.Create(downloadPath)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	_, err = downloader.Download(
+		file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucketName),
+			Key:    aws.String(key),
+		},
+	)
+
+	return err
+}
+
+func DeleteFile(client *s3.S3, bucketName string, fileName string) error {
+	_, err := client.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(fileName),
+	})
+
+	return err
+}
+
+func PresignUrl(client *s3.S3, bucketName string, fileName string) (string, error) {
+	req, _ := client.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(fileName),
+	})
+
+	urlStr, err := req.Presign(15 * time.Minute)
+	if err != nil {
+		return "", err
+	}
+
+	return urlStr, nil
+}
