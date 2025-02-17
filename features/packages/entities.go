7b35c4972eb4baaa1c8382946e6767f83ba4b867
Ulil Amry <ulilamry432@gmail.com>
2025-02-17 12:29:40 +0700

    feat:update field in individual packages entities

--- features/packages/entities.go
+++ features/packages/entities.go
@@ -4,12 +4,11 @@ import "gorm.io/gorm"
 
 type Packages struct {
 	gorm.Model
-	ID          uint                 `gorm:"primaryKey"`
-	Name        string               `gorm:"type:varchar(255);not null"`
-	PricePerKg  float64              `gorm:"type:decimal(10,2);not null"`
-	Description string               `gorm:"type:text"`
-	Cover       string               `gorm:"type:varchar(255)"`
-	Individual  []IndividualPackages `gorm:"foreignKey:PackagesID"`
+	ID          uint    `gorm:"primaryKey"`
+	Name        string  `gorm:"type:varchar(255);not null"`
+	PricePerKg  float64 `gorm:"type:decimal(10,2);not null"`
+	Description string  `gorm:"type:text"`
+	Cover       string  `gorm:"type:varchar(255)"`
 }
 
 func (Packages) TableName() string {
@@ -18,11 +17,9 @@ func (Packages) TableName() string {
 
 type IndividualPackages struct {
 	gorm.Model
-	PackagesID uint     `gorm:"not null"`
-	Name       string   `gorm:"type:varchar(255);not null"`
-	Price      float64  `gorm:"type:decimal(10,2);not null"`
-	Cover      string   `gorm:"type:varchar(255)"`
-	Package    Packages `gorm:"foreignKey:PackagesID"`
+	Name  string  `gorm:"type:varchar(255);not null"`
+	Price float64 `gorm:"type:decimal(10,2);not null"`
+	Cover string  `gorm:"type:varchar(255)"`
 }
 
 func (IndividualPackages) TableName() string {
